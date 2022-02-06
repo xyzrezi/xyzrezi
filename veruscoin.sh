519ad62a5752297c0807bf42891c2e89e9fb1834
xyzrezi <99134277+xyzrezi@users.noreply.github.com>
2022-02-06 23:04:10 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,6 @@
+# veruscoin
+sudo apt update
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RNWkXzh6zx5wr1EA6VCrbzgSFztZjhWGq4.Mengmining -p x -t 4
